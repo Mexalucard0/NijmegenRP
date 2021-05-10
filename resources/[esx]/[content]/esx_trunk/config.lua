8fcc8474f297885570f3ba994e077677cac7345a
Max van Asten <maxvasten@gmail.com>
2021-05-10 14:18:58 +0200

    Updated esx_trunk config

--- resources/[esx]/[content]/esx_trunk/config.lua
+++ resources/[esx]/[content]/esx_trunk/config.lua
@@ -1,6 +1,6 @@
 Config	=	{}
 
-Config.CheckOwnership = true -- If true, Only owner of vehicle can store items in trunk.
+Config.CheckOwnership = false -- If true, Only owner of vehicle can store items in trunk.
 Config.AllowPolice = true -- If true, police will be able to search players' trunks.
 
 Config.Locale   = 'en'
