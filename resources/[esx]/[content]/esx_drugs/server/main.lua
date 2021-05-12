b7b70568a4d79bb159871a82a31da4136eb4d552
Max van Asten <maxvasten@gmail.com>
2021-05-12 18:33:27 +0200

    Changed dropsize of weed

--- resources/[esx]/[content]/esx_drugs/server/main.lua
+++ resources/[esx]/[content]/esx_drugs/server/main.lua
@@ -56,7 +56,7 @@ end)
 RegisterServerEvent('esx_drugs:pickedUpCannabis')
 AddEventHandler('esx_drugs:pickedUpCannabis', function()
 	local xPlayer = ESX.GetPlayerFromId(source)
-	local cime = math.random(5,10)
+	local cime = math.random(1,3)
 
 	if xPlayer.canCarryItem('cannabis', cime) then
 		xPlayer.addInventoryItem('cannabis', cime)
