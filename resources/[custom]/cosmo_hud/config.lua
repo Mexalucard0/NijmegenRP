40404d0f51b1e5c243b18402afd723e179d9cf12
Max van Asten <maxvasten@gmail.com>
2021-05-14 15:11:14 +0200

    Added ingame voice chat + radios

--- resources/[custom]/cosmo_hud/config.lua
+++ resources/[custom]/cosmo_hud/config.lua
@@ -5,5 +5,5 @@ Config.ShowStress = false -- set to true if you want a stress indicator
 Config.ShowSpeedo = true -- set to true if you want speedometer enabled
 Config.ShowVoice = true -- set to false if you want to hide mic indicator
 Config.UnitOfSpeed = "kmh"  -- "kmh" or "mph"
-Config.UseRadio = true -- Shows headset icon instead of microphone if radio is on - REQUIRES "rp-radio"
+Config.UseRadio = false -- Shows headset icon instead of microphone if radio is on - REQUIRES "rp-radio"
 Config.ShowFuel = true -- Show fuel indicator
